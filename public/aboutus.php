a371f22cfdd392ea3aab783bd9da23ccd12f565c
Georges51379 <boutros.georges513@gmail.com>
2024-05-18 18:26:02 +0300

    about us section done

--- public/aboutus.php
+++ public/aboutus.php
@@ -1,22 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php include 'include/head.inc.php'; ?>
-        <title>clothing brand | About Us</title>
-    </head>
+<head>
+    <?php include 'include/head.inc.php'; ?>
+    <title>clothing brand | About Us</title>
+</head>
+<body>
+    <?php
+    include 'include/arrow_to_top.inc.php';
+    include 'include/navbar.inc.php';
+    ?>
 
-    <body>
-<!-- THE INCLUDES SECTION -->
-            <?php   include 'include/arrow_to_top.inc.php';
-                    include 'include/navbar.inc.php';
-            ?>
+    <div class="wrapper">
+        <div class="about-section">
+            <div class="about-text">
+                <h2>About Us</h2>
+                <p>Our vision is to...</p>
+            </div>
+            <div class="about-image">
+                <img src="img/slideshow1.png" alt="Random Image">
+            </div>
+        </div>
+        <div class="random-section">
+            <div class="random-image">
+                <img src="img/slideshow3.png" alt="Random Image">
+            </div>
+            <div class="random-text">
+                <p>Random Text</p>
+            </div>
+        </div>
+    </div><!--END WRAPPER-->
 
-<div class="wrapper">
-
-
-</div><!--END WRAPPER-->
-            <?php include 'include/footer.inc.php';
-            include 'include/progressbar.inc.php'; ?>
-            <script src="js/script.js"></script>
-    </body>
+    <?php include 'include/footer.inc.php'; ?>
+    <?php include 'include/progressbar.inc.php'; ?>
+    <script src="js/script.js"></script>
+</body>
 </html>
