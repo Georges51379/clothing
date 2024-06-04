bb28c0731369777774baac6d7178c87d05b3f6b9
Georges51379 <boutros.georges513@gmail.com>
2024-06-04 09:49:10 +0300

    fixed shop navbar design

--- public/include/shop-navbar.inc.php
+++ public/include/shop-navbar.inc.php
@@ -1,60 +1,41 @@
-<?php require_once '../private/dataProcessing.php'; ?>
-<div class="top-wrapper">
-    <div class="topnav" id="myTopnav">
-        <div class="middle-links">
+<link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+<input type="checkbox" id="check">
+    <header class="shop-header">
+        <label for="check">
+            <i class="fas fa-bars" id="sidebar_btn"></i>
+        </label>
+        <div class="left_area">
+            <h3>clothing<span>brand</span></h3>
+        </div>
+        <div class="right_area">
+            <a href="#" class="logout_btn">Logout</a>
+        </div>
+    </header>
+    <div class="mobile_nav">
+        <div class="nav_bar">
+            <img src="1.png" class="mobile_profile_image" alt="Profile Image">
+            <i class="fa fa-bars nav_btn"></i>
+        </div>
+        <div class="mobile_nav_items">
             <a href="shop.php">welcome user</a>
-            <a href="account.php"><i class="fas fa-user"></i></a>
-            <a href="wishlist.php"><i class="fas fa-heart"></i></a>
-            <a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
-            <a href="track-order.php"><i class="fas fa-truck"></i></a>
-            <a href="order-history.php"><i class="fas fa-box-open"></i></a>
-            <a href="requestproduct.php" title="Request Product"><i class="fas fa-box"></i></a>
-            <a href="login-user.php"><i class="fas fa-sign-in-alt"></i></a>
-            <a href="javascript:void(0);" class="icon" onclick="hamburgerResponsive()">
-            <i class="fa fa-bars"></i>
-        </a>
+            <a href="account.php"><i class="fas fa-user"></i>account</a>
+            <a href="wishlist.php"><i class="fas fa-heart"></i>wishlist</a>
+            <a href="cart.php"><i class="fas fa-shopping-cart"></i>cart</a>
+            <a href="track-order.php"><i class="fas fa-truck"></i>track order</a>
+            <a href="order-history.php"><i class="fas fa-box-open"></i>order history</a>
+            <a href="request-product.php"><i class="fas fa-box"></i>request product</a>
         </div>
-        </div><!---END TOPBAR--> 
-        <div class="shop-bar">
-        <div class="btn-group dropend">
-            <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <?php echo $womenCategory; ?>    
-            </button>
-            <ul class="dropdown-menu dropdown-menu-dark">
-                <li class="dropdown-list"><a href="#" class="dropdown-links">all</a></li>
-                <li class="dropdown-list"><a href="#" class="dropdown-links">shirts</a></li>
-                <li class="dropdown-list"><a href="#" class="dropdown-links">pants</a></li>
-            </ul>
+    </div>
+    <div class="sidebar">
+        <div class="profile_info">
+            <img src="https://i.imgur.com/iQpdHb2.jpg" class="profile_image" alt="Admin Image">
+            <h4>admin name</h4>
         </div>
-            <a href="index.php" class="shop-links">clothing brand</a>
-            <div class="btn-group dropstart">
-                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <?php echo $menCategory; ?>       
-            </button>
-                <ul class="dropdown-menu dropdown-menu-dark">
-                    <li class="dropdown-list"><a href="index.php" class="dropdown-links">all</a></li>
-                    <li class="dropdown-list"><a href="#" class="dropdown-links">shirts</a></li>
-                    <li class="dropdown-list"><a href="#" class="dropdown-links">pants</a></li>
-                </ul>
-            </div>
-        </div>
-        
-</div><!--END TOP WRAPPER--><center>
-<div class="search">
-    <input type="text" class="search-input" placeholder="Search">
-</div></center>
-
-<script>
-      // Hamburger responsive function
-      function hamburgerResponsive() {
-      const topnav = document.getElementById("myTopnav");
-      if (topnav) {
-          if (topnav.className === "topnav") {
-              topnav.className += " responsive";
-          } else {
-              topnav.className = "topnav";
-          }
-      }
-  }
-
-</script>
+        <a href="index.php"><i class="fas fa-home"></i><span>home</span></a>
+        <a href="dashboard.php"><i class="fas fa-tachometer-alt"></i><span>dashboard</span></a>
+        <a href="category.php"><i class="fas fa-list"></i><span>category</span></a>
+        <a href="products.php"><i class="fas fa-boxes"></i><span>product</span></a>
+        <a href="users.php"><i class="fas fa-users"></i><span>users</span></a>
+        <a href="ads.php"><i class="fas fa-ad"></i><span>ads</span></a>
+        <a href="settings.php"><i class="fas fa-cog"></i><span>settings</span></a>
+    </div>
